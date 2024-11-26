863f3fc838d2aad8b1cfee8744f5fa2213200a5b
Ankit Balhara <ankitbalhara012@gmail.com>
2024-11-26 12:26:33 +0530

    Eleminating Password to send in Frontend

--- LoginPanelWithOTP/backend/controllers/profile.controller.js
+++ LoginPanelWithOTP/backend/controllers/profile.controller.js
@@ -1,11 +1,10 @@
 const User = require("../models/Register.model");
 
 const handleProfilePage = async (req, res) => {
-  const UserDetails = await User.findOne({ email: req.userdata.email });
+  const UserDetails = await User.findOne({ email: req.userdata.email }).select("-password");
   if (!UserDetails) {
     return res.status(400).json({ message: "No user Found" });
   }
-  // console.log(req.userdata);
   res
     .status(200)
     .json({ message: "User Data detched successfully", user: UserDetails });
