6716123cf5f054a82488b62d6f2ebe6b27ce4459
Ankit Balhara <ankitbalhara012@gmail.com>
2024-12-20 10:47:54 +0530

    Protected Middleware

--- ChatApp/backend/src/index.js
+++ ChatApp/backend/src/index.js
@@ -6,9 +6,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-
+import cookieParser from "cookie-parser";
 app.use("/api/auth", AuthRoutes);
-
+app.use(cookieParser());
 app.listen(process.env.PORT, () => {
   console.log(`Server is started at Port : ${process.env.PORT}`);
   connectDB();
