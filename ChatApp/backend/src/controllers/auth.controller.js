f77ba25ed1bab96035494a53f066a0aa5a6361c3
Ankit Balhara <ankitbalhara012@gmail.com>
2024-12-20 10:18:34 +0530

    Login Controller

--- ChatApp/backend/src/controllers/auth.controller.js
+++ ChatApp/backend/src/controllers/auth.controller.js
@@ -7,9 +7,9 @@ export const signup = async (req, res) => {
   const { email, password, fullName } = req.body;
 
   try {
-    if(!email || !password || !fullName){
-        return res.status(400).json({message:"All fields are required"})
-  }
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
     if (password.length < 6) {
       return res
         .status(400)
@@ -22,7 +22,7 @@ export const signup = async (req, res) => {
 
     const hashPassword = await bcrypt.hash(password, 10);
 
-    const newUser = new UserModel({ fullName, email, password:hashPassword });
+    const newUser = new UserModel({ fullName, email, password: hashPassword });
 
     if (newUser) {
       // generate jwt token
@@ -39,14 +39,45 @@ export const signup = async (req, res) => {
       return res.status(400).json({ message: "Invalid User Data" });
     }
   } catch (error) {
-    console.log("Error in SignUp",error);
-    res.status(500).json({message:"Internal Server Error"})
+    console.log("Error in SignUp", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 export const login = async (req, res) => {
-  res.send("JAI SHREE RAM");
+  // res.send("JAI SHREE RAM");
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials E" });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(400).json({ message: "Invalid Credentials P" });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in Login Controller :-", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
+
 export const logout = async (req, res) => {
-  res.send("JAI SHREE RAM");
+  // res.send("JAI SHREE RAM");
+  try {
+    res.clearCookie("token", { httpOnly: true, sameSite: "lax" });
+    res.status(200).json({ message: "Logout Successfully" });
+  } catch (error) {
+    console.log("Error in Logout Controller:-", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
