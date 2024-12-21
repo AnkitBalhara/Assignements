73fd72381d2f1954cb9e0abfd194e907502fd0ef
Ankit Balhara <ankitbalhara012@gmail.com>
2024-12-21 08:12:56 +0530

    Message Router

--- ChatApp/backend/src/index.js
+++ ChatApp/backend/src/index.js
@@ -1,14 +1,23 @@
 import express from "express";
 const app = express();
+
 import cookieParser from "cookie-parser";
 app.use(cookieParser());
-import AuthRoutes from "./routes/auth.route.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import { connectDB } from "./DataBase/db.js";
+
 import dotenv from "dotenv";
 dotenv.config();
 
 app.use(express.json());
-app.use("/api/auth", AuthRoutes);
+
+app.use("/api/auth", authRoutes);
+
+app.use("/api/message", messageRoutes);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is started at Port : ${process.env.PORT}`);
   connectDB();
