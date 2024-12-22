a18118991107e97ecf58968f1894ef030c636dc8
Ankit Balhara <ankitbalhara012@gmail.com>
2024-12-22 17:44:19 +0530

    Zustand Update

--- ChatApp/frontend/src/store/useAuthStore.js
+++ ChatApp/frontend/src/store/useAuthStore.js
@@ -6,18 +6,17 @@ export const useAuthStore = create((set) => ({
   isSigningUp: false,
   isLoggingIn: false,
   isUpdatingProfile: false,
-  
+  isCheckingAuth: true,
+
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data });
     } catch (error) {
-      console.log("Error in check Auth :-", error);
+      console.log("Error in Check Auth", error);
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
     }
   },
-
-  isCheckingAuth: true,
 }));
