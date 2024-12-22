ac4418923322cbac3dfea4917b0751b4294d0ced
Ankit Balhara <ankitbalhara012@gmail.com>
2024-12-22 22:11:29 +0530

    Sign Up Page Design

--- ChatApp/frontend/src/App.jsx
+++ ChatApp/frontend/src/App.jsx
@@ -1,22 +1,52 @@
 import React, { useEffect } from "react";
-import Navbar from "./components/Navbar";
-import { Outlet } from "react-router-dom";
-import { useAuthStore } from "./store/useAuthStore";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import SettingPage from "./pages/SettingPage.jsx";
+
+import { useAuthStore } from "./store/useAuthStore.js";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const { authUser, checkAuth } = useAuthStore();
-
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  console.log({ authUser });
-
+  if (!authUser && isCheckingAuth) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
+  console.log(authUser);
   return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/profile"
+            element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />}
+          />
+          <Route path="/setting" element={<SettingPage />} />
+        </Routes>
+
+        <Toaster />
+      </div>
+    </BrowserRouter>
   );
 };
 
--- ChatApp/frontend/src/main.jsx
+++ ChatApp/frontend/src/main.jsx
@@ -3,43 +3,44 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage.jsx";
-import SignUpPage from "./pages/SignUpPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import ProfilePage from "./pages/ProfilePage.jsx";
-import SettingPage from "./pages/SettingPage.jsx";
+// import HomePage from "./pages/HomePage.jsx";
+// import SignUpPage from "./pages/SignUpPage.jsx";
+// import LoginPage from "./pages/LoginPage.jsx";
+// import ProfilePage from "./pages/ProfilePage.jsx";
+// import SettingPage from "./pages/SettingPage.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/signup",
-        element: <SignUpPage />,
-      },
-      {
-        path: "/login",
-        element: <LoginPage />,
-      },
-      {
-        path:"/profile",
-        element:<ProfilePage/>
-      },
-      {
-        path:"/setting",
-        element:<SettingPage/>
-      }
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <HomePage />,
+//       },
+//       {
+//         path: "/signup",
+//         element: <SignUpPage />,
+//       },
+//       {
+//         path: "/login",
+//         element: <LoginPage />,
+//       },
+//       {
+//         path:"/profile",
+//         element:<ProfilePage/>
+//       },
+//       {
+//         path:"/setting",
+//         element:<SettingPage/>
+//       }
+//     ],
+//   },
+// ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <App/>
   </StrictMode>
 );
