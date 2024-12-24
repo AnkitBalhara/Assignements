e6c111d5eb0c00206dc11a631e15c10b4ffa4a7e
Ankit Balhara <ankitbalhara012@gmail.com>
2024-12-24 21:25:16 +0530

    Updated Chat App

--- ChatApp/frontend/src/pages/SignUpPage.jsx
+++ ChatApp/frontend/src/pages/SignUpPage.jsx
@@ -36,7 +36,7 @@ const SignUpPage = () => {
     <div className="min-h-screen grid lg:grid-cols-2">
       {/* left side */}
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
-        <div className="w-full max-w-md space-y-8">
+        <div className="w-full max-w-md space-y-8 mt-3">
           {/* LOGO */}
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
