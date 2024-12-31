583af3812405d3ac23469f1c2c69b6c042144836
Ankit Balhara <ankitbalhara012@gmail.com>
2024-12-31 22:06:11 +0530

    Chat Box animation

--- ChatApp/frontend/src/components/ChatContainer.jsx
+++ ChatApp/frontend/src/components/ChatContainer.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
-import { Loader2 } from "lucide-react";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 const ChatContainer = () => {
