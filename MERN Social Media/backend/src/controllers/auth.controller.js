b997f6936dd56320d12d2a14cbd5d997fbe6888c
Ankit Balhara <ankitbalhara012@gmail.com>
2025-01-04 13:52:54 +0530

    Logout and Login routes in Backend

--- MERN Social Media/backend/src/controllers/auth.controller.js
+++ MERN Social Media/backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import UserModel from "../models/user.model.js";
 
 export const signup = async (req, res) => {
+    // "JAI SHREE RAM"
   const { email, password, fullName } = req.body;
 
   try {
@@ -39,6 +40,44 @@ export const signup = async (req, res) => {
     }
   } catch (error) {
     console.log("Error in SignUp", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const login = async (req, res) => {
+      // "JAI SHREE RAM"
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials E" });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(400).json({ message: "Invalid Credentials P" });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in Login Controller :-", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const logout = async (req, res) => {
+    // "JAI SHREE RAM"
+    try {
+    res.clearCookie("token", { httpOnly: true, sameSite: "lax" });
+    res.status(200).json({ message: "Logout Successfully" });
+  } catch (error) {
+    console.log("Error in Logout Controller:-", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
