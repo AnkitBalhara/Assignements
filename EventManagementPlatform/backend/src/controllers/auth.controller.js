a62d11d458174cf13848e88bb3a5b964706a7676
Ankit Balhara <ankitbalhara012@gmail.com>
2025-02-08 15:05:32 +0530

    Logout Route in Backend

--- EventManagementPlatform/backend/src/controllers/auth.controller.js
+++ EventManagementPlatform/backend/src/controllers/auth.controller.js
@@ -53,18 +53,26 @@ export const login = async (req, res) => {
 
     if (passwordMatch) {
       generateToken(userDetails._id, res);
-      res
-        .status(201)
-        .json({
-          _id: userDetails._id,
-          fullName: userDetails.fullName,
-          email: userDetails.email,
-        });
+      res.status(201).json({
+        _id: userDetails._id,
+        fullName: userDetails.fullName,
+        email: userDetails.email,
+      });
     } else {
       return res.status(400).json({ message: "Invalid Password" });
     }
   } catch (error) {
     console.log("Error in login :-", error);
     res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", { httpOnly: true, sameSite: "lax" });
+    res.status(200).json({ message: "Logout Successfully" });
+  } catch (error) {
+    console.log("Error in Logout Controller:-", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
